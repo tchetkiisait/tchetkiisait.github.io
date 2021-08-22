c1b0833887bc7b19f2b8093543d42ebd576d57e4
tchetkiisait <87891798+tchetkiisait@users.noreply.github.com>
2021-08-22 12:15:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ function getRandomName() {
 }
 
 function getRandomColor() {
-  const color = ["928374", "fb4934", "b8bb26", "fabd2f", "83a598", "d3869b", "8ec07c", "ebdbb2"];
+  const color = ["928374", "fb4934", "b8bb26", "fabd2f", "83a598", "d3869b", "8ec07c", "282828", "cc241d", "98971a", "d79921", "458588", "b16286", "689d6a"];
   return '#' + color[Math.floor(Math.random() * color.length)].toString(16);
 }
 

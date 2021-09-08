bc1449a37e732a62c03963adb49daab51f458e5a
tchetkiisait <87891798+tchetkiisait@users.noreply.github.com>
2021-09-08 14:55:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+var cookies = 0;
+var cursors = 0;
+var save = {
+    cookies: cookies,
+    cursors: cursors,
+    }	
+function cookieClick(number){
+    cookies = cookies + number;
+    document.getElementById("cookies").innerHTML = cookies;
+};
+
+function save(){
+	localStorage.setItem("save",JSON.stringify(save)); 
+};
+
+function load(){
+    var savegame = JSON.parse(localStorage.getItem("save"));
+	if (typeof savegame.cookies !== "undefined") cookies = savegame.cookies;
+	if (typeof savegame.cookies !== "undefined") cookies = savegame.cookies;
+}	
+var cursors = 0;
+
+function buyCursor(){
+    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
+    if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
+        cursors = cursors + 1;                                   //increases number of cursors
+    	cookies = cookies - cursorCost;                          //removes the cookies spent
+        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
+        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
+    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+};
+
+window.onload = load;
+window.setInterval(function(){
+	
+	cookieClick(cursors);
+	
+}, 1000);

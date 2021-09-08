6d80c613e63478fe6d57ee1c86a4a6e9e3e79892
tchetkiisait <87891798+tchetkiisait@users.noreply.github.com>
2021-09-08 17:02:13 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -14,22 +14,24 @@ function save(){
 };
 
 function load(){
-    var savegame = JSON.parse(localStorage.getItem("save"));
-	if (typeof savegame.cookies !== "undefined") cookies = savegame.cookies;
-	if (typeof savegame.cookies !== "undefined") cookies = savegame.cookies;
-}	
+	if(savegame!=null) {
+		var savegame = JSON.parse(localStorage.getItem("save"));
+		if (typeof savegame.cookies !== "undefined") cookies = savegame.cookies;
+		if (typeof savegame.cookies !== "undefined") cookies = savegame.cookies;
+    }	
+}
 var cursors = 0;
 
 function buyCursor(){
-    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
-    if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
-        cursors = cursors + 1;                                   //increases number of cursors
-    	cookies = cookies - cursorCost;                          //removes the cookies spent
-        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
-        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
+    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     
+    if(cookies >= cursorCost){                                   
+        cursors = cursors + 1;                                   
+    	cookies = cookies - cursorCost;                          
+        document.getElementById('cursors').innerHTML = cursors;  
+        document.getElementById('cookies').innerHTML = cookies;  
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
-    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       
+    document.getElementById('cursorCost').innerHTML = nextCost;  
 };
 
 window.onload = load;
